5ebdf08897d35cb9284fe05592eb0a1086daed5a
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-11-06 21:09:45 +0200

    create count-columns function

--- src/app/pages/home/components/product-header/products-header.component.ts
+++ src/app/pages/home/components/product-header/products-header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: `product-header.component.html`,
   styles: []
 })
-export class ProductsHeaderComponent {
+export class ProductsHeaderComponent implements OnInit {
+
+  @Output() columnsCountChange = new EventEmitter<number>();
+
+  itemsShowCount = 12;
+  sort = 'desc'
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  onColumnsUpdated(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum);
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
+  }
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: `./home.component.html`,
-  styles: [
-  ]
+  templateUrl: `./home.component.html`
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  columns = 3
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  onColumnsCountChange(columnsCount: number): void {
+      this.columns = columnsCount
+  }
 
 }
