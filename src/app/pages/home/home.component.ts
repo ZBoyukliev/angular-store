a0757e35e519ef1a947499d84d273e9677c8ae7f
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-11-18 11:49:46 +0200

    add the logic in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,32 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
 import { CartService } from 'src/app/services/cart.service';
+import { StoreService } from 'src/app/services/store.service';
 
 const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 
 @Component({
   selector: 'app-home',
-  templateUrl: `./home.component.html`
+  templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
-
-  columns = 3;
-  rowHeight: number = ROWS_HEIGHT[this.columns];
+export class HomeComponent implements OnInit, OnDestroy {
+  cols = 3;
+  rowHeight: number = ROWS_HEIGHT[this.cols];
+  products: Array<Product> | undefined;
+  count = '12';
+  sort = 'desc';
   category: string | undefined;
+  productsSubscription: Subscription | undefined;
 
-  constructor( private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private storeService: StoreService
+  ) {}
 
   ngOnInit(): void {
-      
+    this.getProducts();
   }
 
-  onColumnsCountChange(columnsCount: number): void {
-      this.columns = columnsCount
-      this.rowHeight = ROWS_HEIGHT[this.columns]
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[colsNum];
+  }
+
+  onItemsCountChange(count: number): void {
+    this.count = count.toString();
+    this.getProducts();
+  }
+
+  onSortChange(newSort: string): void {
+    this.sort = newSort;
+    this.getProducts();
   }
 
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productsSubscription = this.storeService
+      .getAllProducts(this.count, this.sort, this.category)
+      .subscribe((_products) => {
+        this.products = _products;
+      });
   }
 
   onAddToCart(product: Product): void {
@@ -39,4 +66,9 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    if (this.productsSubscription) {
+      this.productsSubscription.unsubscribe();
+    }
+  }
 }
