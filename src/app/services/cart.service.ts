ec85f0d054dd8480c4b11d1f2bbef138ad0c392f
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-11-20 14:53:40 +0200

    common changes

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Cart, CartItem } from '../models/cart.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Cart, CartItem } from '../models/cart.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
   cart = new BehaviorSubject<Cart>({ items: [] });
+
   constructor(private _snackBar: MatSnackBar) {}
 
   addToCart(item: CartItem): void {
@@ -25,10 +25,19 @@ export class CartService {
     this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
   }
 
-  getTotal(items: CartItem[]): number {
-    return items
-      .map((item) => item.price * item.quantity)
-      .reduce((prev, current) => prev + current, 0);
+  removeFromCart(item: CartItem, updateCart = true): CartItem[] {
+    const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    );
+
+    if (updateCart) {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open('1 item removed from cart.', 'Ok', {
+        duration: 3000,
+      });
+    }
+
+    return filteredItems;
   }
 
   removeQuantity(item: CartItem): void {
@@ -55,26 +64,16 @@ export class CartService {
     });
   }
 
-  removeFromCart(item: CartItem, updateCart = true): CartItem[] {
-    const filteredItems = this.cart.value.items.filter(
-      (_item) => _item.id !== item.id
-    );
-
-    if (updateCart) {
-      this.cart.next({ items: filteredItems });
-      this._snackBar.open('1 item removed from cart.', 'Ok', {
-        duration: 3000,
-      });
-    }
-
-    return filteredItems;
-  }
-
   clearCart(): void {
     this.cart.next({ items: [] });
     this._snackBar.open('Cart is cleared.', 'Ok', {
       duration: 3000,
     });
   }
-  
+
+  getTotal(items: CartItem[]): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
 }
